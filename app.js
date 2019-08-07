72e6a3d47d1e8a9247f180e641ae50f85461bc87
cryptodescriptor <36749544+cryptodescriptor@users.noreply.github.com>
2019-08-07 22:44:44 +0100

    Update app.js

--- app.js
+++ app.js
@@ -334,11 +334,34 @@ var frostedPanel = {
 
     // do initial pan
     this.pan();
+  },
+
+  started : false,
+
+  ready : function(callback) {
+    var img = document.body,
+    style = img.currentStyle || window.getComputedStyle(img, false),
+    src = style.backgroundImage.slice(4, -1).replace(/"/g, "");
+    
+    var img = new Image();
+
+    img.onload = function() {
+      if (!this.started) { 
+        this.started = true;
+        callback();
+      }
+    }
+
+    img.src = src;
+
+    if (img.complete) img.onload();
   }
 }
 
-frostedPanel.init();
+frostedPanel.ready(function() {
+  frostedPanel.init();
 
-window.addEventListener("resize", function() {
-  frostedPanel.pan();
+  window.addEventListener("resize", function() {
+    frostedPanel.pan();
+  });
 });
