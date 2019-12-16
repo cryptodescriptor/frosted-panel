3edc250786021b89410adb941ccc48512fcc8f68
cryptodescriptor <36749544+cryptodescriptor@users.noreply.github.com>
2019-12-16 22:05:51 +0000

    Update app.js

--- app.js
+++ app.js
@@ -302,25 +302,8 @@ var frostedPanel = {
       );
   },
 
-  is_mobile : function() {
-    return (typeof window.orientation !== 'undefined') || (navigator.userAgent.indexOf('IEMobile') !== -1);
-  },
-
   get_device_width_and_height : function() {
-    // Mobile versions of Chrome have a problem with innerWidth after rotation.
-    // Use clientWidth + clientHeight instead.
-    var viewportWidth = document.documentElement.clientWidth;
-    var viewportHeight = document.documentElement.clientHeight;
-
-    if (!this.is_mobile()) {
-      var scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
-    } else {
-      var scrollbarWidth = 0;
-    }
-
-    viewportWidth = viewportWidth+scrollbarWidth;
-
-    return [viewportWidth, viewportHeight];
+    return [window.innerWidth, window.innerHeight];
   },
 
   prepare_pan_and_zoom : function() {
