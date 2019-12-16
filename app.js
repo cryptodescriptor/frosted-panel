070b8efd1fd5bf4e315ffbce79e21e54891daf5f
cryptodescriptor <36749544+cryptodescriptor@users.noreply.github.com>
2019-12-16 10:00:55 +0000

    simplify code

--- app.js
+++ app.js
@@ -319,8 +319,7 @@ var frostedPanel = {
       var scrollbarWidth = 0;
     }
 
-    // If scrollbar is visible
-    viewportWidth = (scrollbarWidth > 0) ? (viewportWidth+scrollbarWidth) : viewportWidth;
+    viewportWidth = viewportWidth+scrollbarWidth;
 
     return [viewportWidth, viewportHeight];
   },
